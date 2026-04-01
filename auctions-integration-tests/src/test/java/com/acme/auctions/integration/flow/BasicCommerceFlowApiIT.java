2dd09ad9f1707aad504ffcad31a267ebb41b1935
michal-kudla <michal.kudla@m1k0.com>
2026-04-01 23:03:06 +0200

    Decouple integration test from bootstrap application class

--- auctions-integration-tests/src/test/java/com/acme/auctions/integration/flow/BasicCommerceFlowApiIT.java
+++ auctions-integration-tests/src/test/java/com/acme/auctions/integration/flow/BasicCommerceFlowApiIT.java
@@ -1,24 +1,30 @@
 package com.acme.auctions.integration.flow;
 
-import com.acme.auctions.bootstrap.HexabidApplication;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = HexabidApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+        classes = BasicCommerceFlowApiIT.TestIntegrationApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class BasicCommerceFlowApiIT {
 
     private static final String API_VERSION = "1";
@@ -102,5 +108,12 @@ class BasicCommerceFlowApiIT {
                 .statusCode(org.hamcrest.Matchers.isOneOf(302, 303))
                 .extract()
                 .cookie("JSESSIONID");
+    }
+
+    @EnableScheduling
+    @EntityScan(basePackages = "com.acme.auctions.adapter.out.db")
+    @EnableJpaRepositories(basePackages = "com.acme.auctions.adapter.out.db")
+    @SpringBootApplication(scanBasePackages = "com.acme.auctions")
+    static class TestIntegrationApplication {
     }
 }
